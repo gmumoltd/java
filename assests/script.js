4a5310068d2eb4436ad6fcdbbc084dc0e8c0e862
unknown <gmumoltd@gmail.com>
2026-05-08 16:21:28 +0300

    adddinf atributes and functions and variablea

--- assests/script.js
+++ assests/script.js
@@ -1,1 +1,40 @@
 console.log("Welcome me World")
+
+//variable declaration
+let firstName = "Geoffrey"
+console.log(firstName)
+let your_age = 25;
+console.log(your_age)
+let user_email = "gmumoltd@gmail.com"
+console.log(user_email)
+let mpesaBalance = "464.89 onFuliza"
+console.log(mpesaBalance)
+let fulizaLimit = -446.77
+console.log(fulizaLimit)
+let totalAddition = 5+3
+console.log (totalAddition)
+let totalModulo = 7%2
+console.log(totalModulo)
+let age = 18;
+    if(age < 18){
+        console.log("Minor.come back when you age")
+    }
+    else(age > 18);{
+        console.log("Adult")
+    }
+ //Function
+
+ function greet(){
+    console.log("Hello i am learning JS")
+    alert("THANKYOU")
+   } 
+   //calling the funtion you write the function name 
+    
+   greet()
+ 
+   //PARAMETERS
+   
+function area(length, width){
+console.log(length*width)
+}
+area(20,30)
