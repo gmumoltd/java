bcf8e41c8926d56df6a1e524dbac97dd7ad2e78f
unknown <gmumoltd@gmail.com>
2026-05-12 12:01:21 +0300

    comparative grading system

--- assests/script.js
+++ assests/script.js
@@ -1,40 +1,64 @@
 console.log("Welcome me World")
 
 //variable declaration
-let firstName = "Geoffrey"
-console.log(firstName)
-let your_age = 25;
-console.log(your_age)
-let user_email = "gmumoltd@gmail.com"
-console.log(user_email)
-let mpesaBalance = "464.89 onFuliza"
-console.log(mpesaBalance)
-let fulizaLimit = -446.77
-console.log(fulizaLimit)
-let totalAddition = 5+3
-console.log (totalAddition)
-let totalModulo = 7%2
-console.log(totalModulo)
-let age = 18;
-    if(age < 18){
-        console.log("Minor.come back when you age")
-    }
-    else(age > 18);{
-        console.log("Adult")
-    }
- //Function
+// let firstName = "Geoffrey"
+// console.log(firstName)
+// let your_age = 25;
+// console.log(your_age)
+// let user_email = "gmumoltd@gmail.com"
+// console.log(user_email)
+// let mpesaBalance = "464.89 onFuliza"
+// console.log(mpesaBalance)
+// let fulizaLimit = -446.77
+// console.log(fulizaLimit)
+// let totalAddition = 5+3
+// console.log (totalAddition)
+// let totalModulo = 7%2
+// console.log(totalModulo)
+// let age = 18;
+//     if(age < 18){
+//         console.log("Minor.come back when you age")
+//     }
+//     else(age > 18);{
+//         console.log("Adult")
+//     }
+//  //Function
 
- function greet(){
-    console.log("Hello i am learning JS")
-    alert("THANKYOU")
-   } 
-   //calling the funtion you write the function name 
-    
-   greet()
- 
-   //PARAMETERS
-   
-function area(length, width){
-console.log(length*width)
+//  function greet(){
+//     console.log("Hello i am learning JS")
+//     alert("THANKYOU")
+//    } 
+//    //calling the funtion you write the function name    
+//    greet()
+//    //PARAMETERS 
+// function area(length, width){
+// console.log(length*width)
+// }
+// area(20,30)
+
+
+//grading system
+let marks = -8
+
+console.log(marks);
+if (marks >=85 && marks<=100){
+    console.log("Distiction1")
 }
-area(20,30)
+else if (marks >=75 && marks<=84){
+    console.log("Distiction2")
+}
+else if (marks >=65 && marks<=74){
+    console.log("Credit1")
+}
+else if (marks >=60 && marks<=64){
+    console.log("Credit2")
+}
+else if (marks >=40 && marks<= 59){
+    console.log("Pass")
+}
+else if (marks >=39 && marks<= 0){
+    console.log("Fail")
+}    
+    else {
+    console.log ("Please Input 0-100")
+}
